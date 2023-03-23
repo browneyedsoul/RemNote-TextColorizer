c8dad0fccda584d379aa0e72d3435ea0b2a098bc
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-03-23 13:07:15 +0900

    Update several subdivided options

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -4,88 +4,168 @@ let TextColorizerCSS: string;
 
 async function onActivate(plugin: ReactRNPlugin) {
   const [
+    skinnyHLRD,
     skinnyTextRD,
+    skinnyHLOR,
     skinnyTextOR,
+    skinnyHLYW,
     skinnyTextYW,
+    skinnyHLGR,
     skinnyTextGR,
+    skinnyHLBL,
     skinnyTextBL,
+    skinnyHLVT,
     skinnyTextVT,
-    revertRD,
-    revertOR,
-    revertYW,
-    revertGR,
-    revertBL,
-    revertVT,
+    revertHLRD,
+    revertTextRD,
+    revertHLOR,
+    revertTextOR,
+    revertHLYW,
+    revertTextYW,
+    revertHLGR,
+    revertTextGR,
+    revertHLBL,
+    revertTextBL,
+    revertHLVT,
+    revertTextVT,
   ] = [
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red span.linear-editor-item.whitespace-pre-wrap {
   font-weight: 400;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.linear-editor-item.whitespace-pre-wrap {
   font-weight: 400;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange span.linear-editor-item.whitespace-pre-wrap {
   font-weight: 400;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.linear-editor-item.whitespace-pre-wrap {
   font-weight: 400;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow span.linear-editor-item.whitespace-pre-wrap {
+  font-weight: 400;
+}`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.linear-editor-item.whitespace-pre-wrap {
   font-weight: 400;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green span.linear-editor-item.whitespace-pre-wrap {
+  font-weight: 400;
+}`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.linear-editor-item.whitespace-pre-wrap {
   font-weight: 400;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue span.linear-editor-item.whitespace-pre-wrap {
+  font-weight: 400;
+}`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.linear-editor-item.whitespace-pre-wrap {
+  font-weight: 400;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple span.whitespace-pre-wrap {
+  font-weight: 400;
+}`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.linear-editor-item.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.linear-editor-item.whitespace-pre-wrap {
+  font-weight: 400;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red span.whitespace-pre-wrap {
   font-weight: 550;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange span.whitespace-pre-wrap {
   font-weight: 550;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow span.whitespace-pre-wrap {
   font-weight: 550;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green span.whitespace-pre-wrap{
   font-weight: 550;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue span.whitespace-pre-wrap {
   font-weight: 550;
 }
 `,
     `
-#tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.whitespace-pre-wrap,
-.dark #tile__document .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.whitespace-pre-wrap {
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple {
+  font-weight: 550;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--red.bold span.whitespace-pre-wrap {
+  font-weight: 550;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--orange.bold span.whitespace-pre-wrap {
+  font-weight: 550;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--yellow.bold span.whitespace-pre-wrap {
+  font-weight: 550;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--green.bold span.whitespace-pre-wrap {
+  font-weight: 550;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--blue.bold span.whitespace-pre-wrap {
+  font-weight: 550;
+}
+`,
+    `
+#main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.whitespace-pre-wrap,
+.dark #main .rn-editor__rem__body__text:not(.rem-header--1):not(.rem-header--2):not(.rem-header--3) .highlight-color--purple.bold span.whitespace-pre-wrap {
   font-weight: 550;
 }
 `,
@@ -107,65 +187,124 @@ async function onActivate(plugin: ReactRNPlugin) {
     await plugin.app.registerCSS("text-colorizer", TextColorizerCSS);
   }
   await plugin.settings.registerBooleanSetting({
+    id: "dieting-hl-rd",
+    title: "Reduce font weight in Red Highlight",
+    defaultValue: false,
+  });
+  await plugin.settings.registerBooleanSetting({
+    id: "dieting-hl-or",
+    title: "Reduce font weight in Orange Highlight",
+    defaultValue: false,
+  });
+  await plugin.settings.registerBooleanSetting({
+    id: "dieting-hl-yw",
+    title: "Reduce font weight in Yellow Highlight",
+    defaultValue: false,
+  });
+  await plugin.settings.registerBooleanSetting({
+    id: "dieting-hl-gr",
+    title: "Reduce font weight in Green Highlight",
+    defaultValue: false,
+  });
+  await plugin.settings.registerBooleanSetting({
+    id: "dieting-hl-bl",
+    title: "Reduce font weight in Blue Highlight",
+    defaultValue: false,
+  });
+  await plugin.settings.registerBooleanSetting({
+    id: "dieting-hl-vt",
+    title: "Reduce font weight in Purple Highlight",
+    defaultValue: false,
+  });
+  await plugin.settings.registerBooleanSetting({
     id: "dieting-rd",
-    title: "Reduce Text Weight in Red Highlight",
+    title: "Reduce font weight in Red Text",
     defaultValue: false,
   });
   await plugin.settings.registerBooleanSetting({
     id: "dieting-or",
-    title: "Reduce Text Weight in Orange Highlight",
+    title: "Reduce font weight in Orange Text",
     defaultValue: false,
   });
   await plugin.settings.registerBooleanSetting({
     id: "dieting-yw",
-    title: "Reduce Text Weight in Yellow Highlight",
+    title: "Reduce font weight in Yellow Text",
     defaultValue: false,
   });
   await plugin.settings.registerBooleanSetting({
     id: "dieting-gr",
-    title: "Reduce Text Weight in Green Highlight",
+    title: "Reduce font weight in Green Text",
     defaultValue: false,
   });
   await plugin.settings.registerBooleanSetting({
     id: "dieting-bl",
-    title: "Reduce Text Weight in Blue Highlight",
+    title: "Reduce font weight in Blue Text",
     defaultValue: false,
   });
   await plugin.settings.registerBooleanSetting({
     id: "dieting-vt",
-    title: "Reduce Text Weight in Purple Highlight",
+    title: "Reduce font weight in Purple Text",
     defaultValue: false,
   });
   await plugin.track(async (reactivePlugin) => {
+    const rdHLWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-hl-rd");
+    rdHLWeight === true
+      ? plugin.app.registerCSS("skinny-hl-rd", skinnyHLRD)
+      : plugin.app.registerCSS("skinny-hl-rd", revertHLRD);
+
+    const orHLWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-hl-or");
+    orHLWeight === true
+      ? plugin.app.registerCSS("skinny-hl-or", skinnyHLOR)
+      : plugin.app.registerCSS("skinny-hl-or", revertHLOR);
+
+    const ywHLWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-hl-yw");
+    ywHLWeight === true
+      ? plugin.app.registerCSS("skinny-hl-yw", skinnyHLYW)
+      : plugin.app.registerCSS("skinny-hl-yw", revertHLYW);
+
+    const grHLWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-hl-gr");
+    grHLWeight === true
+      ? plugin.app.registerCSS("skinny-hl-gr", skinnyHLGR)
+      : plugin.app.registerCSS("skinny-hl-gr", revertHLGR);
+
+    const blHLWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-hl-bl");
+    blHLWeight === true
+      ? plugin.app.registerCSS("skinny-hl-bl", skinnyHLBL)
+      : plugin.app.registerCSS("skinny-hl-bl", revertHLBL);
+
+    const vtHLWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-hl-vt");
+    vtHLWeight === true
+      ? plugin.app.registerCSS("skinny-hl-vt", skinnyHLVT)
+      : plugin.app.registerCSS("skinny-hl-vt", revertHLVT);
     const rdWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-rd");
     rdWeight === true
       ? plugin.app.registerCSS("skinny-text-rd", skinnyTextRD)
-      : plugin.app.registerCSS("skinny-text-rd", revertRD);
+      : plugin.app.registerCSS("skinny-text-rd", revertTextRD);
 
     const orWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-or");
     orWeight === true
       ? plugin.app.registerCSS("skinny-text-or", skinnyTextOR)
-      : plugin.app.registerCSS("skinny-text-or", revertOR);
+      : plugin.app.registerCSS("skinny-text-or", revertTextOR);
 
     const ywWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-yw");
     ywWeight === true
       ? plugin.app.registerCSS("skinny-text-yw", skinnyTextYW)
-      : plugin.app.registerCSS("skinny-text-yw", revertYW);
+      : plugin.app.registerCSS("skinny-text-yw", revertTextYW);
 
     const grWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-gr");
     grWeight === true
       ? plugin.app.registerCSS("skinny-text-gr", skinnyTextGR)
-      : plugin.app.registerCSS("skinny-text-gr", revertGR);
+      : plugin.app.registerCSS("skinny-text-gr", revertTextGR);
 
     const blWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-bl");
     blWeight === true
       ? plugin.app.registerCSS("skinny-text-bl", skinnyTextBL)
-      : plugin.app.registerCSS("skinny-text-bl", revertBL);
+      : plugin.app.registerCSS("skinny-text-bl", revertTextBL);
 
     const vtWeight = await reactivePlugin.settings.getSetting<boolean>("dieting-vt");
     vtWeight === true
       ? plugin.app.registerCSS("skinny-text-vt", skinnyTextVT)
-      : plugin.app.registerCSS("skinny-text-vt", revertVT);
+      : plugin.app.registerCSS("skinny-text-vt", revertTextVT);
   });
 }
 
